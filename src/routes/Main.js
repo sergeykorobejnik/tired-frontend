eb6b16e11fdd0f71633aa52b426ad078648f7acf
Sergey_Korobejnik <ksn.u@yandex.ua>
2022-04-09 00:50:42 +0300

    switching requests to another host

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -3,7 +3,7 @@ import ContentContainer from "../components/ContentContainer/ContentContainer";
 
 
 const settings = {
-    root: 'https://tired-frontend-backend.sergeykorobejni.repl.co',
+    root: 'https://tired-frontend-backend.herokuapp.com',
     get: '/parsed-data',
     post: '/filter-state'
 }
